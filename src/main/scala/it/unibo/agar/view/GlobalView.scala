e6d52bf2b1f1d92eeb92e26a028a8df397019609
g-mainardi <giosue.mainardi@studio.unibo.it>
2025-07-23 14:20:30 +0200

    View refactors

--- src/main/scala/it/unibo/agar/view/GlobalView.scala
+++ src/main/scala/it/unibo/agar/view/GlobalView.scala
@@ -1,8 +1,7 @@
 package it.unibo.agar.view
 
-import it.unibo.agar.model.MockGameStateManager
+import it.unibo.agar.model.World
 
-import java.awt.Color
 import java.awt.Graphics2D
 import scala.swing.*
 
@@ -10,8 +9,13 @@ class GlobalView(manager: MockGameStateManager) extends MainFrame:
 
   title = "Agar.io - Global View"
   preferredSize = new Dimension(800, 800)
+  private var current: Option[World] = None
 
   contents = new Panel:
     override def paintComponent(g: Graphics2D): Unit =
       val world = manager.getWorld
       AgarViewUtils.drawWorld(g, world)
+
+  def update(world: World): Unit =
+    current = Some(world)
+    repaint()
--- src/main/scala/it/unibo/agar/view/LocalView.scala
+++ src/main/scala/it/unibo/agar/view/LocalView.scala
@@ -1,6 +1,7 @@
 package it.unibo.agar.view
 
 import it.unibo.agar.model.MockGameStateManager
+import it.unibo.agar.model.World
 
 import java.awt.Graphics2D
 import scala.swing.*
@@ -10,18 +11,26 @@ class LocalView(manager: MockGameStateManager, playerId: String) extends MainFra
   title = s"Agar.io - Local View ($playerId)"
   preferredSize = new Dimension(400, 400)
 
+  private var worldOpt: Option[World] = None
+
+  def updateWorld(update: World): Unit =
+    worldOpt = Some(update)
+    repaint()
+
   contents = new Panel:
     listenTo(keys, mouse.moves)
     focusable = true
     requestFocusInWindow()
 
-    override def paintComponent(g: Graphics2D): Unit =
-      val world = manager.getWorld
-      val playerOpt = world.players.find(_.id == playerId)
-      val (offsetX, offsetY) = playerOpt
-        .map(p => (p.x - size.width / 2.0, p.y - size.height / 2.0))
-        .getOrElse((0.0, 0.0))
-      AgarViewUtils.drawWorld(g, world, offsetX, offsetY)
+    override def paintComponent(g: Graphics2D): Unit = worldOpt match
+      case Some(world) =>
+        val (offsetX, offsetY) = world.players find (_.id == playerId) match
+          case Some(p) => (p.x - size.width / 2.0, p.y - size.height / 2.0)
+          case None    => (0.0, 0.0)
+
+        AgarViewUtils.drawWorld(g, world, offsetX, offsetY)
+      case None =>
+        g.drawString("Loading...", size.width / 2, size.height / 2)
 
     reactions += { case e: event.MouseMoved =>
       val mousePos = e.point
